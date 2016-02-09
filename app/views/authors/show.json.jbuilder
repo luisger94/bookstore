2f702ca359d6f54fa5a8dddd513a9257027c5592
Luis Ortega <lgorte01@louisville.edu>
2016-02-09 11:43:52 -0500

    Added succesfully authors' section

--- app/views/authors/show.json.jbuilder
+++ app/views/authors/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @author, :id, :title, :description, :created_at, :updated_at
+json.extract! @author, :id, :name, :country, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205163859) do
+ActiveRecord::Schema.define(version: 20160208183348) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name",    limit: 255
+    t.string "country", limit: 255
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title",       limit: 255
