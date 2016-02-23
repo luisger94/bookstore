f85239eb651f5835cb1a4264f03cc526af81a0c2
Luis Ortega <lgorte01@louisville.edu>
2016-02-23 15:34:22 -0500

    Login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,20 +1,20 @@
 class AuthenticationController < ApplicationController
   def signup
-  	if request.post? 
-  		if User.register_user(params[:name], params[:email], params[:password])
-  		  flash.now[:notice] = "User registered successfully"
-  		  # after the user has logged in, take them to the books index
-  		  redirect_to books_path
-  	  else
-  	  	# this email is already registered
-  		  flash.now[:notice] = "This email is already registered. Try logging in instead!"
-  	  end
-  	end
+    if request.post? 
+      if User.register_user(params[:name], params[:email], params[:password])
+        flash.now[:notice] = "User registered successfully"
+        # after the user has logged in, take them to the books index
+        redirect_to books_path
+      else
+        # this email is already registered
+        flash.now[:notice] = "This email is already registered. Try logging in instead!"
+      end
+    end
   end
 
   def login
-    if request.post? 
-        User.authenticate(params[:email], params[:password])
+    if request.post?
+      if User.authenticate(params[:email], params[:password])
         flash.now[:notice] = "User logged in successfully"
         # after the user has logged in, take them to the books index
         redirect_to books_path
@@ -22,6 +22,7 @@ class AuthenticationController < ApplicationController
         # this email is already registered
         flash.now[:notice] = "Wrong email or password"
       end
+    end
   end
    
 
