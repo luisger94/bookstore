7a1ac3c4df76dd6a18f824de33cd9e3bdf7b8e33
Luis Ortega <lgorte01@louisville.edu>
2016-02-26 15:19:47 -0500

    uniqueness of email for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 
 	def self.register_user(name, email, password)
 		logger.info("\n\n ^^^^ Registering user with email: #{email.inspect} \n\n")
